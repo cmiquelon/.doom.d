77e2dc53e426159d584ecbddab28c228b7cd2cce
Charles Miquelon <miqueloncharles@gmail.com>
2021-04-18 10:11:20 -0400

    Change font to Fira Code Retina on Linux

--- config.el
+++ config.el
@@ -21,7 +21,9 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "Fira Code" :size (when IS-LINUX 26)))
+(setq doom-font (if IS-MAC
+                    (font-spec :family "Fira Code")
+                  (font-spec :family "Fira Code Retina" :size 24)))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
